2e943772b2df4352c8359c7eaba27067580ec7c4
Richard Cho <96richardohc@gmail.com>
2020-02-11 09:59:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_reader :roster, :student, :grade
+  def initialize(roster)
+    roster ={}
+    @roster=roster
+  end
+  
+  roster = {10 => ["Jeff Baird", "Blake Johnson"]}
+  
+  def add_student(student, grade)
+    if !roster.has_key?(grade) 
+      roster[grade] = []
+      roster[grade] << student
+    else
+      roster[grade] << student
+    end
+  end
+  
+  
+end
